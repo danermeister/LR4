c641b599bc0185bc569eabb5196cf72d96ca45bb
danermeister <70635084+danermeister@users.noreply.github.com>
2021-02-01 19:25:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,107 +5,119 @@
 #include <algorithm>
 using namespace std;
 
-void quicksort(vector<int> &vec, int L, int R)
-{
-    int i, j, mid, piv;
-    i = L;
-    j = R;
-    mid = L + (R - L) / 2;
-    piv = vec[mid];
-    
-    while (i<R || j>L)
+void newVec(vector<int>& vec) {
+    int j;
+    for (int i = 0; i < 30; i++)
     {
-        while (vec[i] < piv)
-            i++;
-        while (vec[j] > piv)
-            j--;
-        
-        if (i <= j)
-        {
-            swap(vec, i, j);
-            i++;
-            j--;
-        }
-        else
-            {
-            if (i < R)
-                quicksort(vec, i, R);
-            if (j > L)
-                quicksort(vec, L, j);
-            return;
-        }
+        j = rand() % 200 - 100;
+        vec.push_back(j);
+
     }
 }
 
+void printVec(vector<int>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+
+void bubbleSort(vector<int>& vec) {
+    int a = 0;
+    int p = 1;
+    for (int i = 0; i < 30; i++)
+    {
+        if (vec[p] > vec[p + 1])
+        {
+            a = vec[p + 1];
+            vec[p + 1] = vec[p];
+            vec[p] = a;
+            i = 0;
+        }
+        p++;
+
+        if (p == 29) p = 0;
+
+
+    }
+}
+
+void insertSort(vector<int>& vec) {
+    int a = 0;
+    int p = 1;
+    for (int i = 0; i < 30; i++)
+    {
+        if (vec[p - 1] > vec[p])
+        {
+            a = vec[p];
+            for (int n = 0; n < p; n++) {
+                vec[p - n] = vec[p - n - 1];
+            }
+            vec[0] = a;
+            i = 0;
+        }
+        p++;
+
+        if (p == 30) p = 1;
+    }
+}
+    
+void quickSort(vector<int>& vec, int low, int high)
+{
+    int i = low;
+    int j = high;
+    int pivot = vec[(i + j) / 2];
+    int temp;
+
+    while (i <= j)
+    {
+        while (vec[i] < pivot)
+            i++;
+        while (vec[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            temp = vec[i];
+            vec[i] = vec[j];
+            vec[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (j > low)
+        quickSort(vec, low, j);
+    if (i < high)
+        quickSort(vec, i, high);
+}
+
+   
+
 
 int main()
-    {
-//    Задача 1.
-        int qwer2[30];
-    int qwer[30];
-        int a=0;
-        int p=1;
-        int n=0;
-        int l=0;
-    for (int i=0; i<30; i++)
-        {
-        qwer2[i] = rand() % 200-100;
-            qwer[i]=qwer2[i];
-            cout<<qwer2[i]<<" ";
-        }
-//     Bubble
-        cout<<endl<<"Puziriki:"<<endl;
-        
-            for (int i=0;i<30;i++)
-            {
-                if (qwer[p] > qwer[p+1])
-                {
-                    a=qwer[p+1];
-                    qwer[p+1]=qwer[p];
-                    qwer[p]=a;
-                    i=0;
-                }
-                p++;
-                n++;
-                if (p==29) p=0;
-            }
-        for (int p=0;p<30;p++){
-            cout<<qwer[p]<<" ";
-        }
-        cout<<"\n"<<"Iteraciy: "<<n<<endl;
-//     Insert
-        for (int i=0;i<30;i++){
-        qwer[i]=qwer2[i];
-        }
-        cout<<"Insert:"<<endl;
-        for (int i=0;i<30;i++)
-        {
-            if (qwer[p-1] > qwer[p])
-                {
-                    a=qwer[p];
-                    for (n=0;n<p;n++){
-                        qwer[p-n]=qwer[p-n-1];
-                    }
-                    qwer[0]=a;
-                    i=0;
-                }
-            p++;
-            l++;
-            if (p==30) p=1;
-        }
-        for (int p=0;p<30;p++){
-            cout<<qwer[p]<<" ";
-        }
-        cout<<"\n"<<"Iteraciy: "<<l<<endl;
-        
-quicksort(qwer2, 0, size.qwer2() - 1);
+{
+    vector<int> qwer1;
+    vector<int> qwer2;
+    vector<int> qwer3;
     
-    for(int i : vec1)
-    {
-        cout << i << endl;
-    }
+    cout << "Bubble sort:" << endl;
+    newVec(qwer1);
+    printVec(qwer1);
+    bubbleSort(qwer1);
+    printVec(qwer1);
+   
+    cout << "Insert sort:" << endl;
+    newVec(qwer2);
+    printVec(qwer2);
+    insertSort(qwer2);
+    printVec(qwer2);
     
-       return 0;
-    }
-    
+    cout << "Quick sort:" << endl;
+    newVec(qwer3);
+    printVec(qwer3);
+    insertSort(qwer3);
+    printVec(qwer3);
 
+
+    return 0;
+}
